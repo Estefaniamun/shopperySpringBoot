f9862e48a581dab27f9fcad72eb2dd4267459e79
estefaniamun <estefaniamunca@gmail.com>
2023-06-04 18:36:05 +0200

    "controladores"

---
+++ src/main/java/com/example/shoperry/controladores/CategoriaController.java
@@ -0,0 +1,5 @@
+package com.example.shoperry.controladores;
+
+public class CategoriaController {
+
+}
---
+++ src/main/java/com/example/shoperry/controladores/DTO.java
@@ -0,0 +1,12 @@
+package com.example.shoperry.controladores;
+
+	import java.util.HashMap;
+
+	@SuppressWarnings("serial")
+	public class DTO extends HashMap<String, Object>{
+
+		public DTO() {
+			super();
+		}
+	}
+
